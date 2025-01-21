d3458f6c104f1cf24cce88179e7723e0fb51b295
Viviana Bautista <lviviana13@gmail.com>
2025-01-20 21:09:38 -0500

    feat: :sparkles: add services, dependency injects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { TestLearnComponent } from './test-learn/test-learn.component';
 import { MapsComponent } from './maps/maps.component';
 import { SectionsComponent } from './pages/sections/sections.component';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { FormsComponent } from './forms/forms.component';
+import { CarService } from './services/car.service';
 
 
 @Component({
@@ -24,6 +25,7 @@ import { FormsComponent } from './forms/forms.component';
 <app-maps></app-maps>
 <app-forms></app-forms>
 <app-sections></app-sections>
+  <p>Car Listing: {{ display }}</p> 
     }
     @else {
       <h1>
@@ -35,6 +37,8 @@ import { FormsComponent } from './forms/forms.component';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  display = '';
+
   title = 'angular-poc';
   isServerRunning = true;
 
@@ -45,6 +49,10 @@ export class AppComponent {
 
   addItem(count: number) {
     this.je = count;
+  }
+
+  constructor(private carService: CarService) {
+    this.display = this.carService.getCars().join(' ⭐️ ');
   }
 
 }
---
+++ src/app/car/car.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-car',
+  imports: [],
+  templateUrl: './car.component.html',
+  styleUrl: './car.component.scss'
+})
+export class CarComponent {
+
+}
